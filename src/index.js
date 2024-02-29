821cf91fe0e680157b0bd522d9614107a1ca55e4
Sergey-Ivanovich <ericnoata@gmail.com>
2024-02-29 15:09:47 +0000

    improved ux and small page redesign

--- src/index.js
+++ src/index.js
@@ -12,21 +12,26 @@ function displayAnswer(response) {
 function handleForm(event) {
   event.preventDefault();
 
+  answerShown.classList.remove("hidden");
   let userPrompt = document.querySelector("#user-prompt");
   let apiKey = `oe3107c03bbf1b061844a8c3d518t9b3`;
-  let apiContext = `you are an experienced travel agent, and is ready to help when a user inputs anything related to travelling, whether that would be a question, asking for suggestions, or just inputing prompt words, always stay on topic and give you answers in a bullet point html format utilizing different elements like <strong> where necessary, do your best to give a full response even if just a single word has been inputted, use these as prompts`;
+  let apiContext = `you are an experienced travel agent, and is
+   ready to help when a user inputs anything related to travelling,
+    whether that would be a question, asking for suggestions, or just
+     inputing prompt words, always stay on topic and give you answers
+      in a bullet point html format utilizing different elements like 
+      <strong> where necessary, do your best to give a full response 
+      even if just a single word has been inputted, use these as prompts, 
+      always try to relate things to giving an answer as locations for 
+      a vacation, REMEMBER, ALWAYS, try to give your answer a 3 separate
+      locations in the prestated bullet point format 
+      as an  easter egg, tell the user a funny joke about eggs when he asks "tell me a yoke"`;
   let apiPrompt = userPrompt.value;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayAnswer);
 
-  new Typewriter(answerShown, {
-    strings: `Generating a Response based on "${userPrompt.value}"...`,
-
-    autoStart: true,
-    delay: 1,
-    cursor: ` `,
-  });
+  answerShown.innerHTML = `⏳ <span class="generating">  Generating a Response based on "${userPrompt.value}" </span>`;
 }
 
 let answerShown = document.querySelector("#reply-box");
