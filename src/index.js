65161937b13f1fe691abd10a1fe36d675ad8f7ec
Sergey-Ivanovich <ericnoata@gmail.com>
2024-02-29 10:20:36 +0000

    added typewriter plugin and basic form handling in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function handleForm(event) {
+  event.preventDefault();
 
+  let answerShown = document.querySelector("#reply-box");
+
+  new Typewriter(answerShown, {
+    strings: `hello turkey is pretty cool i think`,
+    autoStart: true,
+    delay: 1,
+    cursor: ` `,
+  });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", handleForm);
